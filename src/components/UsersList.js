a4fa3c52fbcb2e82dfb582b54ee7e28118aa9225
Klaudia Kieryk <klaudiakieryk123@gmail.com>
2024-01-06 13:22:03 +0100

    Implement async thunk for fetching users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,6 +1,27 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchUsers } from '../store'
+
 const UsersList = () => {
+  const dispatch = useDispatch()
+  const { isLoading, data, error } = useSelector((state) => (
+    state.users
+  ))
+
+  useEffect(()=>{
+    dispatch(fetchUsers())
+  },[])
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>
+  }
+
   return (
-    <div>UsersList</div>
+    <div>{JSON.stringify(data)}</div>
   )
 }
 
